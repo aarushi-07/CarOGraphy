56353aeca0dde7911e2f32c144405fda7d9619a0
aarushi-07 <30468254+aarushi-07@users.noreply.github.com>
2024-02-08 12:08:22 +0530

    update

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
+def about(request):
+    a = "<p>Welcome to CarOGraphy!</p>"
+    return HttpResponse(a)
