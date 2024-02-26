b63b62853296c7eb1acdc3b3e691eecd484a48ed
aarushi-07 <30468254+aarushi-07@users.noreply.github.com>
2024-02-26 23:23:50 +0530

    Revert "user guide changes"

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,6 +6,5 @@ from .views import login_view
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('home/', views.home_view, name='home'),
-    path('user-guide/', views.user_guide, name='user_guide')
 
 ]
