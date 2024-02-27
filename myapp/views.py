6feeda476c78eddfac6c2dcfdc03df1b5fc313fe
aarushi-07 <30468254+aarushi-07@users.noreply.github.com>
2024-02-28 02:52:23 +0530

    Revert "Registration page"

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
 
 from .models import CarUser
-
 
 def login_view(request):
     if request.method == 'POST':
@@ -20,27 +18,5 @@ def login_view(request):
         return render(request, 'login.html')
 
 
-def register_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        confirm_password = request.POST.get('confirm_password')
-
-        if password != confirm_password:
-            return render(request, 'register.html', {'error_message': 'Passwords do not match'})
-
-        if User.objects.filter(email=email).exists():
-            return render(request, 'register.html', {'error_message': 'User with this email already exists'})
-
-        user = User.objects.create_user(username=username,email=email, password=password)
-        user.save()
-
-        user = authenticate(request, username=email, password=password)
-        login(request, user)
-        return redirect('login')  
-    return render(request, 'register.html')
-
-
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request,'home.html')
