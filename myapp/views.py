a79425f15cc29a1b496913017331581806aeb504
VrajPatel1814 <103623658+VrajPatel1814@users.noreply.github.com>
2024-02-27 16:11:22 -0500

    Registration Views Page Created
    A view Page to register the user accessing the website for the first time.

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 from .models import CarUser
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -18,5 +20,27 @@ def login_view(request):
         return render(request, 'login.html')
 
 
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        if password != confirm_password:
+            return render(request, 'register.html', {'error_message': 'Passwords do not match'})
+
+        if User.objects.filter(email=email).exists():
+            return render(request, 'register.html', {'error_message': 'User with this email already exists'})
+
+        user = User.objects.create_user(username=username,email=email, password=password)
+        user.save()
+
+        user = authenticate(request, username=email, password=password)
+        login(request, user)
+        return redirect('login')  
+    return render(request, 'register.html')
+
+
 def home_view(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
