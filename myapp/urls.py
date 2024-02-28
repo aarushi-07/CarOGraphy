f63646cc13ad00a8a704cc8178e9faab7df4d714
Arish Izhar <ether@Arishs-MacBook-Pro.local>
2024-02-27 19:26:47 -0500

    make the forgot password page and view

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,5 +6,6 @@ from .views import login_view
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('home/', views.home_view, name='home'),
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
 
 ]
