da887df94b9d5bd14c6f8a7db44b10c5d3d6affe
Arish Izhar <ether@Arishs-MacBook-Pro.local>
2024-03-11 02:42:45 -0400

    small edits and typos

--- myapp/urls.py
+++ myapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # reset password feature urls 
     # done by Arish 
     path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="python3 password_reset_done"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 
