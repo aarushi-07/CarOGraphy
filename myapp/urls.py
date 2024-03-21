223323e4e6bf9b030feb61e027872ecf2dff29c8
Arish Izhar <ether@Arishs-MacBook-Pro.local>
2024-03-21 18:18:45 -0400

    minor changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('contact-us/', views.contact_us, name='contact_us'),
+    path('user-history/', views.user_history_view, name='user_history'),
 
 
 ]
