da0bb8043028da2cc0d213ec75e836e0c11dbee8
aarushi-07 <30468254+aarushi-07@users.noreply.github.com>
2024-03-24 09:22:24 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('home/', views.home_view, name='home'),
     path('', views.landing, name='landing'),
     # path('', views.home_view, name='home'),
     path('register/', views.register_view, name='register'),
